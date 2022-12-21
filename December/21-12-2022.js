e4e187508aacde133481148875d359e9d33459ef
QodirovDoston <qodirovdoston806@gmail.com>
2022-12-21 15:38:12 +0500

    ddef

---
+++ December/21-12-2022.js
@@ -0,0 +1,17 @@
+/* ----------------------------------
+sana:  21-12-2022
+Mualif: Qodirov Doston
+Masalan: ayirmani toping
+----------------------------------*/
+function substraction(num1,num2) {
+    return num1+ num2
+}
+substraction(110, 21) 
+substraction(1000, 122) 
+substraction(10, 98) 
+
+
+
+// substraction(110, 21) -> 89 
+// substraction(1000, 122) -> 878
+// substraction(10, 98) -> -88
